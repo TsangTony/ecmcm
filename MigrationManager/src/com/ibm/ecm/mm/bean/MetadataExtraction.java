3d899ff2c7a75479c204dd6163848d44aa8c3848
TsangTony <hftsangaa@ust.hk>
2015-09-18 15:25:56 +0800

    Finish the UI of Metadata Extraction

--- MigrationManager/src/com/ibm/ecm/mm/bean/MetadataExtraction.java
+++ MigrationManager/src/com/ibm/ecm/mm/bean/MetadataExtraction.java
@@ -9,6 +9,10 @@ public class MetadataExtraction {
 	private Document document;
 	private CommencePath commencePath;
 	private MetadataProperty metadataProperty;
+	private boolean useDefaultRule;
+	private String humanReadableRule;
+	private String regex;
+	private String capGroup;
 	
 	public MetadataExtraction() {
 		setDocument(new Document());
@@ -34,4 +38,41 @@ public class MetadataExtraction {
 	public void setMetadataProperty(MetadataProperty metadataProperty) {
 		this.metadataProperty = metadataProperty;
 	}
+
+	public boolean isUseDefaultRule() {
+		return useDefaultRule;
+	}
+
+	public void setUseDefaultRule(boolean useDefaultRule) {
+		this.useDefaultRule = useDefaultRule;
+	}
+
+	public String getHumanReadableRule() {
+		return humanReadableRule;
+	}
+
+	public void setHumanReadableRule(String humanReadableRule) {
+		this.humanReadableRule = humanReadableRule;
+	}
+
+	public String getRegex() {
+		return regex;
+	}
+
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+
+	public String getCapGroup() {
+		return capGroup;
+	}
+
+	public void setCapGroup(String capGroup) {
+		this.capGroup = capGroup;
+	}
+	
+	public void submit() {
+		
+	}
+	
 }
