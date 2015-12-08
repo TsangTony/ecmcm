64e8f5b83743297866d0e4e5c91939358dde4e62
mreda <mreda@9.164.132.232>
2015-12-08 17:20:00 +0800

    Adding the new menu for the ACL Extraction functionality

---
+++ MigrationManager/src/com/ibm/ecm/mm/bean/AclExtractionBean.java
@@ -0,0 +1,11 @@
+package com.ibm.ecm.mm.bean;
+
+public class AclExtractionBean {
+	public AclExtractionBean() {
+		System.out.println("Bean Created");
+	}
+
+	public void doAction() {
+		System.out.println("Action fired");
+	}
+}
